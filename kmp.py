2c238ff62e2f08ade7bf009b3daa9d80995384bd
zhtea <zslzone@gmail.com>
2017-07-18 17:10:52 +0800

    add kmp.py

---
+++ kmp.py
@@ -0,0 +1,26 @@
+#KMP  
+def kmp_match(s, p):  
+    m = len(s); n = len(p)  
+    cur = 0#起始指针cur  
+    table = partial_table(p)  
+    while cur<=m-n:  
+        for i in range(n):  
+            if s[i+cur]!=p[i]:  
+                cur += max(i - table[i-1], 1)#有了部分匹配表,我们不只是单纯的1位1位往右移,可以一次移动多位  
+                break  
+        else:  
+            return True  
+    return False  
+  
+#部分匹配表  
+def partial_table(p):  
+    '''''partial_table("ABCDABD") -> [0, 0, 0, 0, 1, 2, 0]'''  
+    prefix = set()  
+    postfix = set()  
+    ret = [0]  
+    for i in range(1,len(p)):  
+        prefix.add(p[:i])  
+        postfix = {p[j:i+1] for j in range(1,i+1)}  
+        ret.append(len((prefix&postfix or {''}).pop()))  
+    return ret  
+   
